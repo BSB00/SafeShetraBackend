9021b7a2a42ad740b18683208fb13cd49ae66f47
BHAVDEEP SINGH <bhavdeepsingh@BHAVDEEPs-MacBook-Air.local>
2025-02-23 19:11:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require("cors");
+
+const mongoose = require('mongoose')
+const User = require("./Models/User")
+const mongo_url="mongodb+srv://bhavdeep:BhavdeepCU@bhavdeepcluster.ybto6.mongodb.net/?retryWrites=true&w=majority&appName=bhavdeepcluster"
+
+const UserRoute= require('./Routes/User')
+const HazardRoute= require('./Routes/Hazard')
+const NotificationRoute= require('./Routes/Notification')
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin:"*",
+    credentials: true,
+}));
+mongoose.connect(mongo_url ,{useNewUrlParser:true})
+
+const con = mongoose.connection
+con.on('open',()=>{
+    console.log("connected.....")
+})
+
+app.get(('/testing', (req, res)=>{
+    res.send("testing")
+}))
+
+app.use('/user',UserRoute)
+app.use('/notification',NotificationRoute)
+app.use('/hazard',HazardRoute)
+
+
+app.listen(8000,()=>{
+    console.log("Server Started....")
+})
